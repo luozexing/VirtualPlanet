a55f12dd34eded2502df79a7f7ed5dd9d19854b1
Luo Zexing <fengye1937@gmail.com>
2015-12-23 21:53:52 +0800

    Change authRemote to authHandler

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -16,85 +16,8 @@ var Handler = function(app) {
  */
 Handler.prototype.entry = function(msg, session, next) {
     console.log("connector.entry begins");
+
     var self = this;
-    var username = msg.username;
-    var password = msg.password;
-    var sessionService = self.app.get("sessionService");
-
-    //already sign in
-    if (sessionService.getByUid(username)) {
-        next(null, {
-            code: 500,
-            error: true,
-            message: "already sign in"
-        });
-        return;
-    }
-
-    self.app.rpc.auth.authRemote.signin(session, username, password, function(){
-        session.bind(username);
-        session.on('closed', onUserLeave(null, self.app));
-        next(null, {
-            code: 200,
-            error: false,
-            message: "sign in successfully"
-        });
-    }, function() {
-        next(null, {
-            code: 500,
-            error: true,
-            message: "username or password incorrect"
-        });
-    });
-
+    
     console.log("connector.entry ends");
 };
-
-Handler.prototype.signup = function(msg, session, next) {
-    var self = this;
-    var username = msg.username;
-    var password = msg.password;
-    var name = msg.name;
-
-    self.app.rpc.auth.authRemote.checkDuplicates(session, username, function(){
-        next(null, {
-            code: 500,
-            error: true,
-            message: "this username already exists"
-        });
-        return;
-    });
-
-    self.app.rpc.auth.authRemote.signup(session, username, password, name, function(){
-        next(null, {
-            code: 200,
-            error: false,
-            message: "sign up successfully"
-        });
-        return;
-    }, function(){
-        next(null, {
-            code: 500,
-            error: true,
-            message: "sign up fails"
-        });
-        return;
-    });
-}
-
-Handler.prototype.exit = function(msg, session, next) {
-    onUserLeave(this.app, session);
-    next(null, {
-        code: 200,
-        error: false,
-        message: "sign out successfully"
-    })
-}
-
-var onUserLeave = function(app, session) {
-    console.log("on user leave");
-    if (!session || !session.uid) {
-        return;
-    }
-    self.app.rpc.auth.authRemote.signout(session, session.uid);
-}
